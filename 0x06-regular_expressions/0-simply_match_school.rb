f6496d91a6c8ef1abc5af59f17c978a2f4932225
Asive Bekezulu <79581892+BaeBischops@users.noreply.github.com>
2024-02-23 21:43:59 +0200

    Update 0-simply_match_school.rb

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,13 +1,1 @@
-#!/usr/bin/env ruby
 
-# Accept the argument from the command line
-input = ARGV[0]
-
-# Define the regular expression to match "School"
-regex = /School/
-
-# Use the scan method to find matches and join them
-matches = input.scan(regex).join
-
-# Output the matches
-puts matches
