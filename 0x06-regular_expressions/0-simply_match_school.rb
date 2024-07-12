bf57f93fba57a185ed159eb784226bce63d3158d
BaeBiscops <dr.bkzee@gmail.com>
2024-07-12 04:01:22 -0700

    search for school with regular expression

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,18 +1,3 @@
 #!/usr/bin/env ruby
-# This was completed by bischops.africa
-
-# Function to process the input and replace 'Sthool' with 'School'
-def process_input(input)
-  input.gsub('Sthool', 'School')
-end
-
-# Read input from ARGV
-input = ARGV[0]
-
-# Process the input and find all occurrences of 'School'
-processed_input = process_input(input)
-output = processed_input.scan(/School/).join
-
-# Print the output if there are any matches; otherwise, print the input itself
-puts output.empty? ? input : output
+puts ARGV[0].scan(/School/)
 
