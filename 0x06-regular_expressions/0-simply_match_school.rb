1ce6868e3f42082970d622ee3e121629fae98119
BaeBiscops <dr.bkzee@gmail.com>
2024-07-12 03:56:55 -0700

    search for school with regular expression

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,14 +1,18 @@
 #!/usr/bin/env ruby
 # This was completed by bischops.africa
 
-input = ARGV[0]
-
-# Function to replace 'Sthool' with 'School'
+# Function to process the input and replace 'Sthool' with 'School'
 def process_input(input)
   input.gsub('Sthool', 'School')
 end
 
+# Read input from ARGV
+input = ARGV[0]
+
 # Process the input and find all occurrences of 'School'
 processed_input = process_input(input)
-puts processed_input.scan(/School/).join
+output = processed_input.scan(/School/).join
+
+# Print the output if there are any matches; otherwise, print the input itself
+puts output.empty? ? input : output
 
