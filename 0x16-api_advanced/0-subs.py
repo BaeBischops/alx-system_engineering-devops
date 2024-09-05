3e4309a1184c656825f1dae04a8d86750e8255ca
Asive Bekezulu <79581892+BaeBischops@users.noreply.github.com>
2024-09-05 18:22:18 +0200

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python3
 """
-    Uses Reddit API to print the number of subscribers of a subreddit
+    Uses Reddit API to print the number of subscribers of a subreddit.
 """
 import requests
 
 
 def number_of_subscribers(subreddit):
     """
-    Get the number of subscribers for a given subreddit
+    Get the number of subscribers for a given subreddit.
     """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'user-agent': 'request'}
+    headers = {'User-Agent': 'request'}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code != 200:
         return 0
 
     data = response.json().get("data")
-    num_subs = data.get("subscribers")
+    if data is None:
+        return 0
 
+    num_subs = data.get("subscribers", 0)
     return num_subs
